904994518c88e2a3c3c0d670931a852ef985c3d1
Daniel-Iliev <S1909011203@sd.uni-vt.bg>
2023-03-28 14:02:06 +0300

    Integrated basic security

---
+++ src/main/java/com/practice/recipes/config/SecurityConfiguration.java
@@ -0,0 +1,36 @@
+package com.practice.recipes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public InMemoryUserDetailsManager user(){
+        return new InMemoryUserDetailsManager(
+                User.withUsername("1")
+                        .password("{noop}1")
+                        .authorities("read")
+                        .build()
+        );
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf(csrf-> csrf.disable())
+                .authorizeHttpRequests(auth-> auth
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+}
